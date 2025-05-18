3acdcfc580235fa581c8831110cdd073842f2911
SiddheshwarKale <siddkale2005@gmail.com>
2025-05-18 22:57:41 +0530

    ex 2 complet

--- src/Main.java
+++ src/Main.java
@@ -3,44 +3,88 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+//        18-may-2025
         Scanner sc=new Scanner(System.in);
+//
+//        System.out.println("1.Addition +");
+//        System.out.println("2.subtraction -");
+//        System.out.println("3.Multiplication *");
+//        System.out.println("4.Division /");
+//        System.out.println("5.Modulo %");
+//
+//        int button=sc.nextInt();
+//
+//        System.out.println("Enter First Number");
+//        int a=sc.nextInt();
+//        System.out.println("Enter Second Number");
+//        int b=sc.nextInt();
+//
+//        switch (button){
+//            case 1:
+//                System.out.println(a+b);
+//                break;
+//            case 2:
+//                System.out.println(a-b);
+//                break;
+//            case 3:
+//                System.out.println(a*b);
+//                break;
+//            case 4:
+//                System.out.println(a/b);
+//                break;
+//            case 5:
+//                System.out.println(a%b);
+//                break;
+//            default:
+//                System.out.println("Error");
+//
+//        }
 
-        System.out.println("1.Addition +");
-        System.out.println("2.subtraction -");
-        System.out.println("3.Multiplication *");
-        System.out.println("4.Division /");
-        System.out.println("5.Modulo %");
+//EX.2: Ask the user to enter the number of the month & print the name of the month.
+//        for ex.-for '1'print 'january','2' print 'februry' & so on.
 
-        int button=sc.nextInt();
+        int Choice= sc.nextInt();
 
-        System.out.println("Enter First Number");
-        int a=sc.nextInt();
-        System.out.println("Enter Second Number");
-        int b=sc.nextInt();
-
-        switch (button){
+        switch (Choice){
             case 1:
-                System.out.println(a+b);
+                System.out.println("January");
                 break;
+
             case 2:
-                System.out.println(a-b);
+                System.out.println("february");
                 break;
             case 3:
-                System.out.println(a*b);
+                System.out.println("march");
                 break;
             case 4:
-                System.out.println(a/b);
+                System.out.println("april");
                 break;
             case 5:
-                System.out.println(a%b);
+                System.out.println("may");
                 break;
-            default:
-                System.out.println("Error");
+            case 6:
+                System.out.println("JUN");
+                break;
+            case 7:
+                System.out.println("july");
+                break;
+            case 8:
+                System.out.println("august");
+                break;
+            case 9:
+                System.out.println("september");
+                break;
+            case 10:
+                System.out.println("octomber");
+                break;
+            case 11:
+                System.out.println("navember");
+                break;
+            case 12:
+                System.out.println("december");
+                break;
 
         }
-
-
-
-
     }
 }
