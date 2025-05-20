ef7bccf8fbf52df7e27d9ff56e40f3fc1c90c27e
SiddheshwarKale <siddkale2005@gmail.com>
2025-05-20 09:34:11 +0530

    7} Que

--- src/Main.java
+++ src/Main.java
@@ -218,14 +218,24 @@ public class Main {
 //        1234
 //        12345
 
+//
+
+
+//        Q7 Print the Pattern    #Inverted half pyramid with numbers
+
+//        12345
+//        1234
+//        123
+//        12
+//        1
+
         int n=5;
-        for(int i=1;i<=n;i++){
+        for(int i=n;i>=1;i--){
             for(int j=1;j<=i;j++){
                 System.out.print(j);
             }
             System.out.println();
         }
-
 
 
 
