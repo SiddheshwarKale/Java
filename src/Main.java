ebd9231b4027f487e8357255f1c029177d21d7f8
SiddheshwarKale <siddkale2005@gmail.com>
2025-05-20 08:30:59 +0530

    3} Que

--- src/Main.java
+++ src/Main.java
@@ -154,16 +154,30 @@ public class Main {
 //        *   *
 //        *****
 
-        int n=4;
-        int m=5;
+//        int n=4;
+//        int m=5;
+//
+//        for( int i=1;i<=n;i++){
+//            for(int j=1;j<=m;j++){
+//                if(i == 1||j == 1||i == n||j == m){
+//                    System.out.print("*");
+//                }else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
 
-        for( int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(i == 1||j == 1||i == n||j == m){
-                    System.out.print("*");
-                }else {
-                    System.out.print(" ");
-                }
+//        Q3  Print the pattern   # half pyramid
+//        *
+//        **
+//        ***
+//        ****
+
+        int n =4;
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
             }
             System.out.println();
         }
