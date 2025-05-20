93fe4728829b2691a39fdad305670a09f52f487e
SiddheshwarKale <siddkale2005@gmail.com>
2025-05-20 10:18:09 +0530

    9} Que

--- src/Main.java
+++ src/Main.java
@@ -245,16 +245,37 @@ public class Main {
 //        4  5  6
 //        7  8  9  10
 //        11 12 13 14 15
+//
+//        int n=5;
+//        int num=1;
+//
+//        for(int i=1;i<=n;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print(num+" ");
+//                num++;
+//            }
+//            System.out.println();
+//        }
+
+
+//        Q9 print the pattern    #0-1 Triangle
+//        1
+//        01
+//        101
+//        0101
+//        10101
 
         int n=5;
-        int num=1;
-
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(num+" ");
-                num++;
-            }
-            System.out.println();
+                int sum=i+j;
+                if(sum%2==0){
+                    System.out.print("1");
+                }else {
+                    System.out.print("0");
+                }
+
+            }System.out.println();
         }
 
 
