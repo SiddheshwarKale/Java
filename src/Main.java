c34c3a890612bf6130dd99cc6e57f05b78aaa46d
SiddheshwarKale <siddkale2005@gmail.com>
2025-05-21 11:27:18 +0530

    Practice for loops

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.net.SocketTimeoutException;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
@@ -265,21 +266,81 @@ public class Main {
 //        0101
 //        10101
 
-        int n=5;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                int sum=i+j;
-                if(sum%2==0){
-                    System.out.print("1");
-                }else {
-                    System.out.print("0");
-                }
+//        int n=5;
+//        for(int i=1;i<=n;i++){
+//            for(int j=1;j<=i;j++){
+//                int sum=i+j;
+//                if(sum%2==0){
+//                    System.out.print("1");
+//                }else {
+//                    System.out.print("0");
+//                }
+//
+//            }System.out.println();
+//        }
 
+
+//        practice 21-may-2025
+
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+//        #2
+
+        System.out.println("..................................");
+
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
             }System.out.println();
         }
 
+//        #3
+        System.out.println("..................................");
+        int num=1;
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+
+        System.out.println("..................................");
+
+//        #4
+     for(int i=1;i<=4;i++){
+         for(int j=1;j<=4;j++){
+             System.out.print("*");
+         }
+         System.out.println();
+     }
 
 
+        System.out.println("..................................");
+
+//     #5
+
+        int n=4;
+        int m=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if( i==1 || j==1 || i==n || j==m){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
 
     }
 }
