9f9c4aa6634d365a8982f7bf1f790904e196583b
SiddheshwarKale <siddkale2005@gmail.com>
2025-05-22 22:52:45 +0530

    13} Que

--- src/Main.java
+++ src/Main.java
@@ -411,13 +411,33 @@ public class Main {
 // 4 4 4 4
 //5 5 5 5 5
 
-        int n =5;
+//        int n =5;
+//        for(int i=1;i<=n;i++){
+//            for(int j=1;j<=n-i;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=1;j<=i;j++){
+//                System.out.print(i + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+//        Q13} Print the Pattern     #palindromic pattern
+//      1
+//     212
+//    32123
+//   4321234
+//  543212345
+
+        int n=5;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n-i;j++){
                 System.out.print(" ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print(i + " ");
+            }for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }for(int j=2;j<=i;j++){
+                System.out.print(j);
             }
             System.out.println();
         }
